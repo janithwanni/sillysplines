9a36fcada324d3140274a77f6d38005e7a1d5ae8
janithwanni <janithcwanni@gmail.com>
2025-11-04 12:55:40 +1100

    Chore: set up working devtools check

--- R/sillysplines.R
+++ R/sillysplines.R
@@ -1,15 +1,24 @@
-#' <Add Title>
+#' Create a box to draw silly spline
 #'
-#' <Add Description>
+#' This creates a simple htmlwidget to be used
+#'
+#' @param initial The initial set of coordinates used to draw the spline, needs to be sorted by x axis
+#' @param width width of container in pixels
+#' @param height height of container in pixels
+#' @param elementId The id to be used for the container element
 #'
 #' @import htmlwidgets
 #'
 #' @export
-sillysplines <- function(message, width = NULL, height = NULL, elementId = NULL) {
-
+sillysplines <- function(
+  initial,
+  width = NULL,
+  height = NULL,
+  elementId = NULL
+) {
   # forward options using x
   x = list(
-    message = message
+    message = initial
   )
 
   # create widget
@@ -40,13 +49,29 @@ sillysplines <- function(message, width = NULL, height = NULL, elementId = NULL)
 #' @name sillysplines-shiny
 #'
 #' @export
-sillysplinesOutput <- function(outputId, width = '100%', height = '400px'){
-  htmlwidgets::shinyWidgetOutput(outputId, 'sillysplines', width, height, package = 'sillysplines')
+sillysplinesOutput <- function(outputId, width = '100%', height = '400px') {
+  htmlwidgets::shinyWidgetOutput(
+    outputId,
+    'sillysplines',
+    width,
+    height,
+    package = 'sillysplines'
+  )
 }
 
 #' @rdname sillysplines-shiny
 #' @export
 renderSillysplines <- function(expr, env = parent.frame(), quoted = FALSE) {
-  if (!quoted) { expr <- substitute(expr) } # force quoted
+  if (!quoted) {
+    expr <- substitute(expr)
+  } # force quoted
   htmlwidgets::shinyRenderWidget(expr, sillysplinesOutput, env, quoted = TRUE)
+}
+
+sillysplines_html <- function(...) {
+  htmltools::tags$div(
+    ...,
+    id = "app",
+    style = "display: flex; justify-content: center; align-items: center; width: 50%; flex-direction: column;"
+  )
 }
--- inst/htmlwidgets/sillysplines.js
+++ inst/htmlwidgets/sillysplines.js
@@ -1,28 +1,21 @@
 HTMLWidgets.widget({
+  name: "sillysplines",
 
-  name: 'sillysplines',
+  type: "output",
 
-  type: 'output',
-
-  factory: function(el, width, height) {
-
+  factory: function (el, width, height) {
     // TODO: define shared variables for this instance
 
     return {
-
-      renderValue: function(x) {
-
+      renderValue: function (x) {
         // TODO: code to render the widget, e.g.
-        el.innerText = x.message;
-
+        console.log(x);
+        el.innerHTML = x;
       },
 
-      resize: function(width, height) {
-
+      resize: function (width, height) {
         // TODO: code to re-render the widget with a new size
-
-      }
-
+      },
     };
-  }
+  },
 });
